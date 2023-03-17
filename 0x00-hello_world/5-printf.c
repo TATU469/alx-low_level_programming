33fd24fed24801cf082a212f506cab774a8b3744
TATU469 <alextatu2014@gmail.com>
2023-03-16 18:16:03 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - describes the function
+ *
+ * Return: returns 0 when the code is clean
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
