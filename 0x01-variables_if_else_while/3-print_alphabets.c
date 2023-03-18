b08014290b3ba430e5a00e460f4be589504f8811
TATU469 <alextatu2014@gmail.com>
2023-03-18 06:00:58 -0700

    print lower and upper alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - describes the function
+ *
+ * Return: returns 0 when code is correct
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
