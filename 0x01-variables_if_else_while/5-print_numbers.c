279bb00c17210434c216fe4f0c6bd913008cadb8
TATU469 <alextatu2014@gmail.com>
2023-03-18 07:03:15 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - decribes the function
+ *
+ * Return: returns 0 when the code is correct
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
