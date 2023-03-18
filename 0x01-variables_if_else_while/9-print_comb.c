add7f3820b9b13de7f33c16318cdec459b9da4b2
TATU469 <alextatu2014@gmail.com>
2023-03-18 11:29:13 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main - descibes function
+ *
+ * Return: returns 0 when the code is correct
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{     putchar(i + '0');
+		if (i != 9)
+		{ putchar(',');
+		  putchar(' ');	
+		}
+       	}
+	putchar('\n');
+	return (0);
+}
