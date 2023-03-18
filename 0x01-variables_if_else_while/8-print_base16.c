0cc536a80e5d848da188067ccbf28c15552aee65
TATU469 <alextatu2014@gmail.com>
2023-03-18 08:27:56 -0700

    printing numbers and alphabet

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - decribes function
+ *
+ * Return: returns 0 when the code is correct
+ */
+
+int main(void)
+{
+	int i;
+	char k;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (k = 'a'; k < 'f'; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
