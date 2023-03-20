736e6df5f992a3ab0981f18ea2f0e731c61a5328
TATU469 <alextatu2014@gmail.com>
2023-03-20 14:52:27 -0700

    territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,16 +11,14 @@ int main(void)
 
 	int b;
 
-	long int c;
-
 	long long int d;
 
 	float x;
 
-	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(x));
 	return (0);
 }
