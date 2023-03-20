7770bda1b414138d832391e543c66e98726797ea
TATU469 <alextatu2014@gmail.com>
2023-03-20 07:17:08 -0700

    coding my love

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,7 +22,7 @@ int main(void)
 				putchar(',');
 				putchar((k / 10) + '0');
 				putchar((k % 10) + '0');
-				if (i != 98 || k != 99)
+				if (i != 98)
 				{
 					putchar(',');
 					putchar(' ');
