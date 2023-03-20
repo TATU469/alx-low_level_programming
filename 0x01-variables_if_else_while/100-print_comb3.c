0c1eebdfd4dfa3472124e6f1f9ea92b9b2b829a3
TATU469 <alextatu2014@gmail.com>
2023-03-20 03:30:29 -0700

    comnbination of brain and material

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: success (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (k = 1; k < 10; k++)
+		{
+			if (i < k && i != k)
+			{
+				putchar(i + '0');
+				putchar(k + '0');
+				if (i + k != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
