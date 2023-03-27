6c263f523fbb5ec6ab588ca07b63e5b1a84ba71b
TATU469 <alextatu2014@gmail.com>
2023-03-27 14:30:56 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - counts string
+ *
+ * @s: parameter to be checked
+ *
+ * Return: counts length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
